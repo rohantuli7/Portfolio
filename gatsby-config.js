dc51875f9fcc719306fd121ce06e6b884307b35a
rohantuli7 <rohann.tulii@gmail.com>
2022-11-24 20:06:57 +0000

    adding files

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ module.exports = {
     title: 'Rohan Tuli',
     description:
       'Rohan Tuli.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://rohantuli7.github.io/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    // twitterUsername: '@bchiang7',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'RohanTuli',
+        short_name: 'RohanTuli',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
